91a11f3d02cf1c5f2a1985309bc78bc862ae63f0
Brad Pinter <brad.pinter@gmail.com>
2014-05-16 15:12:14 -0500

    Phusion images

--- phusion/base/configure.sh
+++ phusion/base/configure.sh
@@ -9,6 +9,9 @@ cp /build/config/timezone.conf /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
 ## Install ntp runit service.
+mkdir -p /var/log/ntpd/
+cp /build/config/ntp-sylogd.conf /var/log/ntpd/config
+
 mkdir -p /etc/service/ntpd/log
 cp /build/runit/ntp /etc/service/ntpd/run
 cp /build/runit/log /etc/service/ntpd/log/run
