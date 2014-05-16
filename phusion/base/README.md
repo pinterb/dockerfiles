9d9ea80e672c317948921594b347ea6ce2c048a4
Brad Pinter <brad.pinter@gmail.com>
2014-05-16 12:03:46 -0500

    Phusion images

---
+++ phusion/base/README.md
@@ -0,0 +1,10 @@
+# pinterb/phusion-base
+
+A base docker image based on phusion/baseimage.  Currently this is an Ubuntu 14.04 image.
+
+Features include:   
+- some handy packages installed   
+- ntp installed with new timezone   
+
+To pull this image:
+`docker pull pinterb/phusion-base`
--- phusion/base/configure.sh
+++ phusion/base/configure.sh
@@ -7,3 +7,8 @@ set -x
 cp /build/config/ntp.conf /etc/ntp.conf
 cp /build/config/timezone.conf /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
+
+## Install ntp runit service.
+mkdir /etc/service/ntpd
+cp /build/runit/ntp /etc/service/ntpd/run
+touch /etc/service/ntpd/down
