65234ceaf1e19c8d00dc3f05477aeb84a6c2c4d4
Brad Pinter <brad.pinter@gmail.com>
2014-05-17 12:59:08 -0500

    Phusion images

--- phusion/base/prepare.sh
+++ phusion/base/prepare.sh
@@ -3,12 +3,12 @@ set -e
 source /build/buildconfig
 set -x
 
-## Create a user for the svlogd.
+## Create a user for logging.
 addgroup --gid 9998 log
-adduser --uid 9998 --gid 9998 --disabled-password --gecos "Logger" log
+adduser --uid 9998 --gid 9998 --disabled-password --no-create-home --gecos "Logger" log
 usermod -L log
 
-## Create a user for the web app.
+## Create a user for applications you may want to run.
 addgroup --gid 9999 app
 adduser --uid 9999 --gid 9999 --disabled-password --gecos "Application" app
 usermod -L app
