68b144c10cb2707c7cb8efd3b075d3e92e81cc5b
Brad Pinter <brad.pinter@gmail.com>
2014-05-19 12:41:43 -0500

    Python image

--- phusion/python/packages.sh
+++ phusion/python/packages.sh
@@ -11,3 +11,9 @@ apt-get install -y python-pip python3-pip
 
 ## install virtualenv
 apt-get install -y python-virtualenv virtualenvwrapper python-tox pbundler
+
+## install Sphinx documentation tools
+apt-get install -y python-sphinx python3-sphinx python-docutils python3-docutils
+
+## install more Sphinx documentation tools
+apt-get install -y python-sphinx.issuetracker python-sphinxcontrib-httpdomain python-sphinxcontrib.blockdiag python-sphinxcontrib.nwdiag python-sphinxcontrib.seqdiag python-sphinxcontrib.spelling python-sphinxcontrib-docbookrestapi
