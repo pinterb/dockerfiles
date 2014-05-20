101a743a9bd1f3885fd8dc61ff9ea2f2c7044aa4
Brad Pinter <brad.pinter@gmail.com>
2014-05-20 09:39:52 -0500

    Phusion Base image

--- phusion/base/prepare.sh
+++ phusion/base/prepare.sh
@@ -9,9 +9,9 @@ adduser --uid 9998 --gid 9998 --disabled-password --no-create-home --gecos "Logg
 usermod -L log
 
 ## Create a user for applications you may want to run.
-addgroup --gid 9999 app
-adduser --uid 9999 --gid 9999 --disabled-password --gecos "Application" app
-usermod -L app
-mkdir -p /home/app/.ssh
-chmod 700 /home/app/.ssh
-chown app:app /home/app/.ssh
+# addgroup --gid 9999 app
+# adduser --uid 9999 --gid 9999 --disabled-password --gecos "Application" app
+# usermod -L app
+# mkdir -p /home/app/.ssh
+# chmod 700 /home/app/.ssh
+# chown app:app /home/app/.ssh
