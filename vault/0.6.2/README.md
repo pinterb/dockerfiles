26d79de01344f0ebd2b4f4bb7737a9d1723ff516
Brad Pinter <brad.pinter@gmail.com>
2016-10-11 14:36:22 +0000

    bump vault version

---
+++ vault/0.6.2/README.md
@@ -0,0 +1,28 @@
+# pinterb/vault  
+
+[`pinterb/vault`][1] is a [docker][2] image that bundles the following:  
+* **[Vault v0.6.2][3]:** A tool that secures, stores, and tightly controls access to tokens, passwords, certificates, API keys, and other secrets in modern computing.
+
+## Details
+* This image only exposes the /vault/config directory from the base HashiCorp image.
+
+## Usage 
+This image can easily be extended.  But to run vault:
+
+````
+docker run -it --rm \
+	-v $(LOCAL_CONFIG_DIR):/vault/config \
+	--cap-add=IPC_LOCK \
+	pinterb/vault:0.6.2 server
+		
+````
+
+## Misc. Info 
+* Latest version: 0.6.2  
+* Built on: 2016-10-11T14:34:28UTC   
+* Base image: vault:0.6.1   
+
+
+[1]: https://hub.docker.com/r/pinterb/vault/   
+[2]: https://docker.com 
+[3]: https://www.vaultproject.io/  
---
+++ vault/0.6.2/docker-entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# vim: filetype=sh:tabstop=2:shiftwidth=2:expandtab
+
+#
+# Define default Variables.
+#
+USER="dev"
+GROUP="dev"
+
+SH_C='sudo -E sh -c'
+
+UID_ACTUAL=$(id -u ${USER})
+GID_ACTUAL=$(id -g ${GROUP})
+
+UID_NAMED=${PUID:-1000}
+GID_NAMED=${PGID:-1000}
+
+#if [ ! "$(id -u dev)" -eq "$PUID" ]; then usermod -o -u "$PUID" dev ; fi
+#if [ ! "$(id -g dev)" -eq "$PGID" ]; then groupmod -o -g "$PGID" dev ; fi
+
+#
+# Display settings on standard out.
+#
+echo ""
+echo "named settings"
+echo "=============="
+echo ""
+echo "  Username:        ${USER}"
+echo "  Groupname:       ${GROUP}"
+echo "  UID actual:      ${UID_ACTUAL}"
+echo "  GID actual:      ${GID_ACTUAL}"
+echo "  UID preferred:   ${UID_NAMED}"
+echo "  GID preferred:   ${GID_NAMED}"
+echo ""
+
+#
+# Change UID / GID of named user.
+#
+echo "Updating UID / GID... "
+if [[ "$UID_ACTUAL" -ne "$UID_NAMED" || "$GID_ACTUAL" -ne "$GID_NAMED" ]]; then
+  echo "  * Change user / group"
+  deluser "$USER"
+  addgroup -g "$GID_NAMED" "$GROUP"
+  adduser -u "$UID_NAMED" -D -H -G "$GROUP" -h /config -s /bin/bash "$USER"
+  echo "    - user / group changed."
+
+  echo "  * Set owner and permissions for old uid/gid files"
+  find / -user "$UID_ACTUAL" -exec chown "$USER" {} \;
+  find / -user "$GID_ACTUAL" -exec chown "$GROUP" {} \;
+  echo "    - file permissions changed."
+else
+  echo "  * UID & GID matched...nothing to do."
+fi
+echo ""
+echo ""
+
+
+# run swagger-cli 
+sudo --user "$USER" /bin/terraform "$@"
